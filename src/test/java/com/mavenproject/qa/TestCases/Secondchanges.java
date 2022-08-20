cccfea61efcf9eff9ff1e194f6c6f5d2263afea1
AasimKazi007 <kaziaasim880@gmail.com>
2022-08-20 16:40:28 +0530

    this is second try

---
+++ src/test/java/com/mavenproject/qa/TestCases/Secondchanges.java
@@ -0,0 +1,9 @@
+package com.mavenproject.qa.TestCases;
+
+public class Secondchanges {
+	
+	//changes some code
+	//proper in sequence order
+	//excution of the test cases properly in line to line
+
+}
